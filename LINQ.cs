16cfb927be3421d83d590adc6aea5d646a623324
Opilane TTHK <opilane@tthk.ee>
2023-03-22 09:40:16 +0200

    Create LINQ.cs

---
+++ LINQ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Töö
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+            Intersect();
+            Take();
+            
+        }
+        public static void Intersect()
+        {
+            //programm leiab millises osakonnas töötavad mõlemas osakonnas
+            string[] employeesInDeptA = { "Mari", "Mart", "Albert", "Siim" };
+            string[] employeesInDeptB = { "Albert", "Jüri", "Jaanus", "Mart" };
+
+            var result = employeesInDeptA.Intersect(employeesInDeptB);
+
+            Console.WriteLine("Ühised töötajad osakonnas A ja B:");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+        public static void Take()
+        {
+            //Võtab kolm esimest nime ja näitab konsoolis
+            Console.WriteLine("");
+            var Scores = new[] { "Liis", "Toomas", "Martin", "Artur", "Kaspar", "Aleks" };
+
+            var newScores = Scores.Take(3);
+
+            foreach (var newScore in newScores)
+            {
+                Console.WriteLine(newScore);
+            }
+        }
+    }
+}   
